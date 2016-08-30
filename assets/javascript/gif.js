af12e6eca8fe85ef60c09ec5c6166efd61ec3563
Matt Holland <mtholland10@gmail.com>
2016-08-30 17:34:26 -0500

    Initial commit

---
+++ assets/javascript/gif.js
@@ -0,0 +1,6 @@
+/**
+ * Program: gif.js
+ * Created: 08/30/2016 by Matt Holland
+ * Located: desktop/bootcamp/homework/giftastic/assets/javascript
+ * Purpose: Provide gameplay code for 6th assignment - AJAX
+ */
